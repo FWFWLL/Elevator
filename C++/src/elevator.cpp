d75a2bbd80b3fa461b8629f2f1473eb0d268ac4f
FWFWLL <ffl@vivaldi.net>
2021-11-21 13:41:37 +0100

    Rename queue.h -> queue.hpp

--- C++/src/elevator.cpp
+++ C++/src/elevator.cpp
@@ -1,15 +1,8 @@
 #include <cstddef>
 #include <iostream>
-#include "queue.h"
+#include "queue.hpp"
 
 int main() {
-	Queue q;
-	q.enqueue(20);
-	q.enqueue(5);
-	q.enqueue(7);
-	q.enqueue(9);
-	std::cout << "Queue front: " << q.dequeue() << std::endl;
-	std::cout << "Queue front after dequeue: " << (q.front) -> data << std::endl;
-	std::cout << "Queue rear: " << (q.rear) -> data << std::endl;
+	Queue destinationQueue;
 	return 0;
 }
